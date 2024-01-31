a93a13a6533bf4c3ea5859f6664f01c4bba65f25
MakKam26 <kamzra@googlemail.com>
2024-01-31 14:29:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# matrixint-array
---
+++ matrixInteger.java
@@ -0,0 +1,39 @@
+public class matrixInteger{
+
+
+public static void main(String[] arug){
+
+int [][] matrixIntExample = {
+
+    {0,1,2,3,4},
+
+    {4,5,6,7,8},
+  
+};
+
+int [] intArray3 = matrixIntExample[0];
+int [] intArray4 = matrixIntExample[1];
+
+int total = intArray3[4] * intArray4[1];
+
+System.out.println(total);
+
+
+
+
+
+
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+}
